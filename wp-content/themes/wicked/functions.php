ae5057c8bea72483395dd3a905ba7a2a3dc882e9
Steve <steve.popoola@simplestream.com>
2013-05-08 11:33:46 +0100

    Added social buttons to each single page post

--- wp-content/themes/wicked/functions.php
+++ wp-content/themes/wicked/functions.php
@@ -16,4 +16,25 @@ function wicked_favicon() {
 }
 add_action('wp_head', 'wicked_favicon');
 
+// Add social media links
+function wicked_linklove($content) {
+    if(is_single()) {
+        $content .= '<div class="linklove">
+        Did you love this post? Tell everyone you know, right now!
+        <!-- AddThis Button BEGIN -->
+        <div class="addthis_toolbox addthis_default_style ">
+        <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+        <a class="addthis_button_tweet"></a>
+        <a class="addthis_button_pinterest_pinit"></a>
+        <a class="addthis_counter addthis_pill_style"></a>
+        </div>
+        <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+        <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4dcd378b3ab75319"></script>
+        <!-- AddThis Button END -->
+    </div>';
+    }
+    return $content;
+}
+
+add_filter('thematic_post','wicked_linklove', 90);
 ?>
